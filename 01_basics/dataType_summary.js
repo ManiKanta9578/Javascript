7cb206539c278b6573c09f98356cd307a99a45bb
ManiKanta9578 <manikanta9578@gmail.com>
2024-05-10 12:14:29 +0530

    stack and heap

--- 01_basics/dataType_summary.js
+++ 01_basics/dataType_summary.js
@@ -30,6 +30,32 @@ const arr = ["Apple", "Mango", "Orange"];
 const obj = { name: 'Mani', age: 26, status: true };
 const fun = function () { console.log("Hello world") };
 
-console.log(typeof arr);
-console.log(typeof obj);
-console.log(typeof fun);
+// console.log(typeof arr);
+// console.log(typeof obj);
+// console.log(typeof fun);
+
+/* ------------------- Stack and Heap ----------------------- */
+
+// types of memory allocation => stack and Heap
+
+// Primitive values stored in stack and Non Primitive values stored in Heap
+
+let name_1 = "Manikanta";
+let name_2 = name_1;
+
+name_2 = "Mani";
+
+console.log(name_1);
+console.log(name_2);  // Primitive values won't change in original values it will give copy of value
+
+const person = {
+    email: "manikanta@gmail.com",
+    mobile: 987654321
+}
+
+const person_1 = person;
+
+person_1.email = 123456789;
+
+console.log(person)
+console.log(person_1); // Non primitive values are change in original values

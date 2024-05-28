aaff1e77c0dc23e4f4b27d29fc65e0a4188b6a6a
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-05-28 12:52:54 +0000

    operators

--- 03_basics/02_expressions&operators.js
+++ 03_basics/02_expressions&operators.js
@@ -7,3 +7,54 @@
 // A unary operator requires a single operand, either before or after the operator:
 
 // For example, x++ or ++x. The operator operand form is called a prefix unary operator, and the operand operator form is called a postfix unary operator. ++ and -- are the only postfix operators in JavaScript — all other operators, like !, typeof, etc. are prefix.
+
+/* ----------------typeOf -------------------- */
+
+
+const myFun = new Function("5 + 2");
+const shape = "round";
+const size = 1;
+const foo = ["Apple", "Mango", "Orange"];
+const today = new Date();
+
+typeof myFun; // returns "function"
+typeof shape; // returns "string"
+typeof size; // returns "number"
+typeof foo; // returns "object"
+typeof today; // returns "object"
+typeof doesntExist; // returns "undefined"
+typeof null; //returns object
+
+// console.log(typeof window.length);
+
+
+/* ----------------in -------------------- */
+
+// Arrays
+const trees = ["redwood", "bay", "cedar", "oak", "maple"];
+0 in trees; // returns true
+3 in trees; // returns true
+6 in trees; // returns false
+"bay" in trees; // returns false
+// (you must specify the index number, not the value at that index)
+"length" in trees; // returns true (length is an Array property)
+
+// built-in objects
+"PI" in Math; // returns true
+const myString = new String("coral");
+"length" in myString; // returns true
+
+// Custom objects
+const mycar = { make: "Honda", model: "Accord", year: 1998 };
+"make" in mycar; // returns true
+"model" in mycar; // returns true
+
+
+/* ----------------instanceof -------------------- */
+
+const theDay = new Date(1995, 12, 17);
+if (theDay instanceof Date) {
+  // statements to execute
+}
+
+console.log(this);

31f6d3558e4a8a6fc31090018ddf614729c98eae
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-05-31 12:50:12 +0000

    regex

---
+++ 03_basics/04_regex.js
@@ -0,0 +1,4 @@
+// Regular expressions are patterns used to match character combinations in strings. 
+// In JavaScript, regular expressions are also objects.
+
+// These patterns are used with the exec() and test() methods of RegExp, and with the match(), matchAll(), replace(), replaceAll(), search(), and split() methods of String.

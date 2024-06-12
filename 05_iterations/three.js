ca41fa113595be15853f46ec6754e0383431e779
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-06-12 07:16:41 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,16 @@
+//for of
+
+["", "", ""]
+[{}, {}, {}]
+
+const arr = [1,2,3,4,5,6];
+
+for(let num of arr){
+    console.log(num);
+}
+
+const greeting = "Hello world!";
+
+for(let char of greeting){
+    console.log(char);
+}
